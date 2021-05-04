3701dcae311d7a751e860c7dd12724ff826b34a4
unknown <petergvillasis@gmail.com>
2021-05-04 10:35:50 +0800

    setup created module controller as default view

--- application/controllers/Modules.php
+++ application/controllers/Modules.php
@@ -1,6 +1,8 @@
 <?php
     class Modules extends CI_Controller{
+        
         public function loadpage(){
+
             $module = "loginpage";
             if(!file_exists(APPPATH.'views/modules/'.$module.'.php')){
                 sho_404();
@@ -8,11 +10,8 @@
 
             $data['title'] = "Security Login";
             $this->load->loadpage('templates/header');
-            $this->load->loadpage('modules/'.$module,$data);
+            $this->load->loadpage('modules/'.$module, $data);
             $this->load->loadpage('templates/footer');
-
 
         }
     }
-
-?>
