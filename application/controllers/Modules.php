31bf56dbcd171995bc254de8f819db46c29a62c5
unknown <petergvillasis@gmail.com>
2021-05-04 09:15:32 +0800

    create function to load the modules

---
+++ application/controllers/Modules.php
@@ -0,0 +1,16 @@
+<?php
+    class Modules exrends CI_Controller{
+        public function loadpage(){
+            $module = "loginpage";
+            if(!file_exists(APPPATH.'views/modules/'.$module.'.php')){
+                sho_404();
+            }
+
+            $data['title'] = "Security Login";
+            $this->load->loadpage('templates/header');
+            $this->load->loadpage('modules/'.$module,$data);
+            $this->load->loadpage('templates/footer');
+            
+
+        }
+    }
