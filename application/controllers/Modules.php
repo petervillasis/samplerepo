1422c6698a40bfdbf6b2ca29298a534cb4a08f69
unknown <petergvillasis@gmail.com>
2021-05-04 09:18:11 +0800

    create modules folder and module file under views

--- application/controllers/Modules.php
+++ application/controllers/Modules.php
@@ -1,5 +1,5 @@
 <?php
-    class Modules exrends CI_Controller{
+    class Modules extends CI_Controller{
         public function loadpage(){
             $module = "loginpage";
             if(!file_exists(APPPATH.'views/modules/'.$module.'.php')){
@@ -10,7 +10,7 @@
             $this->load->loadpage('templates/header');
             $this->load->loadpage('modules/'.$module,$data);
             $this->load->loadpage('templates/footer');
-            
+
 
         }
     }
